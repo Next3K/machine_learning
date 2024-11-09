62402cfc4b88a98457b9a8b5f5dac323158d0f5d
Pawel <236691@edu.p.lodz.pl>
2024-11-09 17:04:06 +0100

    init commit

---
+++ Model.py
@@ -0,0 +1,9 @@
+class Model:
+    class_attribute = "some value"
+
+    def __init__(self, k: int, mask: [bool]):
+        self.k = k
+        self.mask = mask
+
+    def method_name(self):
+        return f"Attributes: {self.k} and {self.mask}"
