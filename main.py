007047643ff9e2cfd3abdf491f7c354c00ae0a59
Pawel <236691@edu.p.lodz.pl>
2024-11-10 16:44:06 +0100

    added hyperparameter optimization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import random
 from datetime import datetime
 import json
 import time
@@ -8,6 +9,26 @@ import json
 import pandas as pd
 
 API_KEY = "api-key"
+
+
+class Knn:
+    def __init__(self, k, mask):
+        self.mask = mask
+        self.k = k
+        self.train_data = None
+
+    @staticmethod
+    def metric(a, b, mask: [int]):
+        # TODO impl
+        return 1.0
+
+    def predict(self, x) -> int:
+        # TODO impl
+        return 3
+
+    def train(self, dataframe):
+        # TODO impl
+        self.train_data = dataframe
 
 
 def scrap_to_json():
@@ -81,16 +102,42 @@ def extract_from_json(directory: str):
     df.to_csv("extracted_movies.csv", index=False)
 
 
-if __name__ == '__main__':
-    # scrap_to_json()
-    extract_from_json("movies")
-
-    train = pd.read_csv("train.csv", sep=';', header=None)
-    train.columns = ['id', 'user_id', 'movie_id', "grade"]
-
+def fill_task_csv(model: {int, Knn}):
+    # TODO implement
     task = pd.read_csv("task.csv", sep=';', header=None)
     task.columns = ['id', 'user_id', 'movie_id', "grade"]
 
+
+if __name__ == '__main__':
+    # scrap_to_json()
+    # extract_from_json("movies")
+
+    train = pd.read_csv("train.csv", sep=';', header=None)
+    train.columns = ['id', 'user_id', 'movie_id', "grade"]
     extracted_movies = pd.read_csv("extracted_movies.csv")
+    dataframe_per_user = {user_id: group_df for user_id, group_df in train.groupby('user_id')}
+
+    # find the best possible knn for every user
+    KNNs: {int, Knn} = {}
+    for user_id, dataframe in dataframe_per_user.items():
+        best_knn: Knn = None
+        best_score: float = .0
+        for k in [1, 3, 5, 7, 11]:
+            for i in range(20):
+                mask = [1 if i in
+                             random.sample(range(13), random.randint(3, 8))
+                        else 0 for i in range(13)]
+
+                knn = Knn(k=k, mask=mask)
+                knn.train(train)
+                # TODO evaluate knn with cross validation etc
+                current_score = 0
+                if current_score > best_score:
+                    best_score = current_score
+                    best_knn = knn
+        KNNs[user_id] = best_knn
+
+    # fill task.csv
+    fill_task_csv(KNNs)
 
     print("Done!")
