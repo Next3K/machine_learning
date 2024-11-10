59ce74e8b4f50d58c6ef02350dad89e5de0ef2eb
Pawel <236691@edu.p.lodz.pl>
2024-11-10 12:25:27 +0100

    added overview

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 import json
 import time
 import csv
@@ -6,13 +7,13 @@ import os
 import json
 import pandas as pd
 
-API_KEY = "none"
+API_KEY = "your-api-key"
 
 
-def scrap_to_json():
-    data = pd.read_csv("movie.csv", sep=';', header=None)
+def scrap_to_json(directory: str):
+    data = pd.read_csv("movie.csv")
     data.columns = ['1', '2', '3']
-    ids = data['2'].tolist()
+    ids = data['2'].toSet()
 
     url_template = "https://api.themoviedb.org/3/movie/{}?api_key=" + API_KEY
     headers = {
@@ -25,7 +26,7 @@ def scrap_to_json():
             response = requests.get(url, headers=headers)
             response.raise_for_status()
             movie_data = response.json()
-            file_path = os.path.join("movies", f"movie_{movie_id}.json")
+            file_path = os.path.join(directory, f"movie_{movie_id}.json")
             with open(file_path, "w") as json_file:
                 json.dump(movie_data, json_file, indent=4)
             print(f"Saved data for movie ID {movie_id}")
@@ -36,8 +37,7 @@ def scrap_to_json():
             print(f"Error for movie ID {movie_id}: {err}")
 
 
-def extract_from_json():
-    directory = "movies"
+def extract_from_json(directory: str):
     data = []
     for filename in os.listdir(directory):
         if filename.endswith(".json"):
@@ -51,21 +51,22 @@ def extract_from_json():
                 release_date = movie_data.get("release_date")
                 revenue = movie_data.get("revenue")
                 runtime = movie_data.get("runtime")
-                title = movie_data.get("title")
                 vote_average = movie_data.get("vote_average")
                 vote_count = movie_data.get("vote_count")
+                overview = movie_data.get("overview")
+
                 data.append({
                     "genres": ", ".join(genres),
                     "budget": budget,
                     "popularity": popularity,
                     "production_companies": ", ".join(production_companies),
                     "production_countries": ", ".join(production_countries),
-                    "release_date": release_date,
+                    "release_date": datetime.strptime(release_date, "%Y-%m-%d").year,
                     "revenue": revenue,
                     "runtime": runtime,
-                    "title": title,
                     "vote_average": vote_average,
-                    "vote_count": vote_count
+                    "vote_count": vote_count,
+                    "overview": overview
                 })
 
     df = pd.DataFrame(data)
@@ -73,8 +74,8 @@ def extract_from_json():
 
 
 if __name__ == '__main__':
-    # scrap_to_json()
-    # extract_from_json()
+    # scrap_to_json("movies")
+    # extract_from_json("movies")
 
     train = pd.read_csv("train.csv", sep=';', header=None)
     train.columns = ['id', 'user_id', 'movie_id', "grade"]
@@ -85,4 +86,3 @@ if __name__ == '__main__':
     extracted_movies = pd.read_csv("extracted_movies.csv")
 
     print("Done!")
-
